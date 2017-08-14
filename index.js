a40e92b1142bca7b64af07644b54ffd724103339
adam-bubela <adam.bubela@explore.com>
2017-08-14 18:24:45 +0200

    cleanup server code add asdmin notiffication (user entred/left)

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-var app = require('express')();
-var express = require('express');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-var users = [];
+var 
+    app = require('express')()
+    express = require('express'),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    port = process.env.PORT || 3000,
+    users = [];
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', (req,res) => res.sendFile(__dirname + '/index.html'));
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-    });
+io.on('connection', (socket)=>{
+
+  socket.on('chat message', (msg) => io.emit('chat message', msg));
   
-    socket.on('mousemove',function(msg){
-      for(let user in users){
-        if(users[user].nick == msg.nick){
-          users[user].pageX = msg.pageX;
-          users[user].pageY = msg.pageY; 
+  socket.on('mousemove',(msg) => {
+    for(let index in users)
+        if(users[index].nick == msg.nick){
+          users[index].pageX = msg.pageX;
+          users[index].pageY = msg.pageY; 
         }
-      }
-      io.emit(`mousemove`, users);
-    });
+    io.emit('mousemove', users);
+  });
 
-  socket.on('adduser',function(msg){
-    users.push(msg); 
+  socket.on('adduser',(msg)=>{
+    users.push(msg);
+    io.emit('chat message', `<span>-- User <b>${msg.nick}</b> joined chat --</span>`);
   });
     
-  socket.on('removeuser',function(nick){
-      for(let index in users){
-        if(users[index].nick == nick){
-          users.splice(index,1);
-          console.log('to delete:',users[index], nick);
-          io.emit('removeuser', nick);
-          return;
-        }
-      }
+  socket.on('removeuser',(nick)=>{
+      for(let index in users)
+          if(users[index].nick == nick){
+            users.splice(index,1);
+            console.log('to delete:',users[index], nick);
+            io.emit('removeuser', nick);
+            io.emit('chat message', `<span>-- User <b>${nick}</b> left chat --</span>`);
+            return;
+          } 
   });
+
 });
 
+http.listen(port, ()=>console.log('listening on *:' + port));
 
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
